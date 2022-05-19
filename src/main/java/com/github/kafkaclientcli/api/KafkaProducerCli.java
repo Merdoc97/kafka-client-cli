c5c2b3e3459c0667f085bb64987511c4117ed98e
Igor <merdoc97@gmail.com>
2022-05-19 11:12:49 +0300

    imports

--- src/main/java/com/github/kafkaclientcli/api/KafkaProducerCli.java
+++ src/main/java/com/github/kafkaclientcli/api/KafkaProducerCli.java
@@ -11,7 +11,6 @@ import org.springframework.stereotype.Component;
 import picocli.CommandLine;
 
 import java.io.File;
-import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.util.Arrays;
 import java.util.Map;
